4bd23d3cee18b202a2d3388ef05f6d27d8e6a3ce
willall1 <wallman1@umbc.edu>
2026-03-09 09:18:54 -0400

    Removed unnecessary comments

--- Task_1.s
+++ Task_1.s
@@ -1,12 +1,11 @@
 .section .bss
 .globl ram
-.lcomm ram, 256 # Reserve 256 bytes of RAM (uninitialized memory)
+.lcomm ram, 256
 
 .section .text
-.globl fill_ram # Make function visible to C program
+.globl fill_ram
 
 fill_ram:
-# Store FFh into RAM locations 50H - 58H using direct addressing
 
 movb $0xFF, ram+0x50
 movb $0xFF, ram+0x51
@@ -18,6 +17,6 @@ movb $0xFF, ram+0x56
 movb $0xFF, ram+0x57
 movb $0xFF, ram+0x58
 
-ret # Return control back to C program
+ret
 
 .section .note.GNU-stack,"",@progbits
